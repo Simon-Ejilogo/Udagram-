399179b439b2a14a21c0e06c8c624dc981814693
simon <simlogo17@gmail.com>
2022-08-25 09:52:17 +0100

    dayob branch test

--- .circleci/ansible/roles/configure-server/files/myenv.sh
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -1,9 +1,9 @@
-export TYPEORM_CONNECTION=postgress
-export TYPEORM_DATABASE=project3devops
+export TYPEORM_CONNECTION=postgres
+export TYPEORM_DATABASE=postgres
 export TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
-export TYPEORM_HOST=project3devops.ctrfhnut1ifl.us-east-1.rds.amazonaws.com
+export TYPEORM_HOST=database-3.c3roiozlob6z.us-east-1.rds.amazonaws.com
 export TYPEORM_MIGRATIONS=./src/migrations/*.ts
 export TYPEORM_MIGRATIONS_DIR=./src/migrations
-export TYPEORM_PASSWORD=project3devops
+export TYPEORM_PASSWORD=postgres
 export TYPEORM_PORT=5432
-export TYPEORM_USERNAME=project3devops
+export TYPEORM_USERNAME=postgres
